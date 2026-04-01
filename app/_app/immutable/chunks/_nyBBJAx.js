7e5801f45e828e5a2c775a9878ca82342fbaa12c
Andy Miller <rhuk@mac.com>
2026-04-01 17:40:06 -0600

    Rebuild with base-aware navigation across all routes

--- app/_app/immutable/chunks/_nyBBJAx.js
+++
@@ -1,1 +0,0 @@
-import{x as p,E as t}from"./DGAaiBia.js";import{B as c}from"./D2qqwuyp.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
---
+++ app/_app/immutable/entry/start.DffXb25p.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/C_baqTX8.js";import{x as t}from"../chunks/ByS-kEkO.js";export{t as load_css,r as start};
