b1560daa4d8e0049a610e2db897ef4223b1555eb
Andy Miller <rhuk@mac.com>
2026-04-17 00:23:53 +0100

    updated with latest admin-next for X-API-Token support
    Signed-off-by: Andy Miller <rhuk@mac.com>

--- app/_app/immutable/entry/start.DADb0xGW.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BCkzH9us.js";import{x as t}from"../chunks/CI8WXGcG.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.DrDVC23D.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CfuExEj9.js";import{x as t}from"../chunks/QkUz9qTz.js";export{t as load_css,r as start};
