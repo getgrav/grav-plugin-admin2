755b8b95d355b1e0a8e5cff8cc3bbf2e9ab90c83
Andy Miller <rhuk@mac.com>
2026-04-20 07:31:34 +0100

    v2.0.0-beta.10: surface API error detail on install failures, rebundle
    Install-failure toasts in AddPluginModal / AddThemeModal now include the
    API's error detail (unmet Grav/PHP version, incompatible constraints,
    or mid-install partial state) instead of a generic "Failed to install"
    message.
    
    Requires grav-plugin-api >= beta.10.

--- app/_app/immutable/entry/start.DJ7Lzmg0.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CEAVA3Ga.js";import{x as t}from"../chunks/DxTxlXVq.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.uKmofuk1.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DwrJg_sg.js";import{x as t}from"../chunks/4-JQk31U.js";export{t as load_css,r as start};
