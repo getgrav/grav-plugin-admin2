ebe98ee7504a1e62b72b7e10aadaf595adedd1f4
Andy Miller <rhuk@mac.com>
2026-04-26 18:38:52 +0100

    updated admin-next bundle with selectize with keys support
    Signed-off-by: Andy Miller <rhuk@mac.com>

--- app/_app/immutable/entry/start.Cqwxe0LQ.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DK2Tc5bj.js";import{w as t}from"../chunks/BIeABD_n.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.CskxXW_F.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CbPMQ9cZ.js";import{w as t}from"../chunks/Brh5x0KJ.js";export{t as load_css,r as start};
