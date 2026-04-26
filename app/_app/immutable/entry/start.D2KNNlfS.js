624eb13dc5e6171c14ded768dee416a8a677cbbc
Andy Miller <rhuk@mac.com>
2026-04-26 11:58:58 +0100

    build: refresh admin-next bundle with sticky-toolbar fixes

--- app/_app/immutable/entry/start.D2KNNlfS.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/dx2WT9Gz.js";import{w as t}from"../chunks/DxqGvBF6.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.z-vI3qeh.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/05bCt-5N.js";import{w as t}from"../chunks/BNMY13e_.js";export{t as load_css,r as start};
