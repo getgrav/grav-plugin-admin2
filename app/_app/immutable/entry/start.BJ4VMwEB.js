43ce11cff3689a6e4b58e308e52c39962c96d333
Andy Miller <rhuk@mac.com>
2026-04-27 11:24:45 +0100

    move languages to admin2 plugin
    Signed-off-by: Andy Miller <rhuk@mac.com>

--- app/_app/immutable/entry/start.BJ4VMwEB.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/C5Y7uitO.js";import{w as t}from"../chunks/DfVJpGzZ.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.Djfb7HqO.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Ba0kywSv.js";import{w as t}from"../chunks/B-xrTVn5.js";export{t as load_css,r as start};
