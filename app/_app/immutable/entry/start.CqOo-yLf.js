1ec13bd8d336559e586815acd9b16a99d527bc91
Andy Miller <rhuk@mac.com>
2026-04-27 15:37:03 +0100

    fix(2fa): keep enrollment section in pending stage after generate
    Effect mirroring props into local stage was clobbering the freshly-set
    'pending' stage when the in-flight 'busy' flag flipped back to false.
    Rebuilt admin-next bundle.
    
    Requires grav-admin-next ad092b8.

---
+++ app/_app/immutable/entry/start.CqOo-yLf.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CTXo0Edc.js";import{w as t}from"../chunks/BTxCAHhQ.js";export{t as load_css,r as start};
--- app/_app/immutable/entry/start.DYfsgDqk.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/krg1-a8j.js";import{w as t}from"../chunks/CFsnP4x-.js";export{t as load_css,r as start};
