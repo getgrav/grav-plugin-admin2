9278670cdfe005da542f26e04824f36a9045fb1c
Andy Miller <rhuk@mac.com>
2026-06-15 18:12:15 -0600

    Rebuild admin2 bundle: theme custom fields (#3) + sidebar badge coalescing (admin2#42)

--- app/_app/immutable/entry/start.BtYi1paP.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BFzNazjj.js";import{x as t}from"../chunks/Be-D797t.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.CGBnyTw5.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/cmjOtMEh.js";import{x as t}from"../chunks/B5BB_VPb.js";export{t as load_css,r as start};
