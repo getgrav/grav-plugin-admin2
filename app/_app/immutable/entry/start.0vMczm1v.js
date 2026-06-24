fc1cd0269a22f2aff37ab64b54f23ff24d0758a8
Andy Miller <rhuk@mac.com>
2026-06-24 11:38:43 -0600

    Rebuild bundle: show full server URL in login/setup field (admin2#58)

--- app/_app/immutable/entry/start.0vMczm1v.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BhP4ludh.js";import{x as t}from"../chunks/wsx9lDTF.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.oVBTIXlO.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Clr5p-2m.js";import{x as t}from"../chunks/BOYg6VV7.js";export{t as load_css,r as start};
