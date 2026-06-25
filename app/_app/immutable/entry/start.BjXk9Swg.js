7fc1d71f27eb865a7e8decb44ab35ae199ce66d3
Andy Miller <rhuk@mac.com>
2026-06-25 13:20:47 -0600

    Rebuild admin2 bundle from admin-next main
    Rebuilt after merging all Twig-in-content UI (report, page-editor banner, profile
    selector, content scan, report route links) plus the interleaved admin2 fixes, so
    the shipped bundle matches the current admin-next main source.

---
+++ app/_app/immutable/entry/start.BjXk9Swg.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/myPcB7Tz.js";import{x as t}from"../chunks/6jeU5jgE.js";export{t as load_css,r as start};
--- app/_app/immutable/entry/start.Dxcs1XNb.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/jmytiO_0.js";import{x as t}from"../chunks/D4YktXTT.js";export{t as load_css,r as start};
