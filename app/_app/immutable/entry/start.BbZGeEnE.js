0cb656940a2c656850d1d445cdba305321387f33
Andy Miller <rhuk@mac.com>
2026-06-29 08:55:26 -0600

    Clear resolved block from the Twig-in-Content report on allowlist add
    Rebuild the admin bundle with optimistic removal of a block row once it is added to the Twig sandbox allowlist.
    
    Fixes #85

--- app/_app/immutable/entry/start.BbZGeEnE.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BPBpt4gy.js";import{x as t}from"../chunks/C77nbukD.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.Ch6lBhjn.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Dc3k16RD.js";import{x as t}from"../chunks/BVCqteLk.js";export{t as load_css,r as start};
