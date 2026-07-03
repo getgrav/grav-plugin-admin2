4ec105b87aeb1bc84bd73999f4d50ddec52cdd0f
Andy Miller <rhuk@mac.com>
2026-07-02 23:59:42 -0600

    Add media metadata editing UI (admin2#99)
    Rebuild admin-next bundle with the media metadata editor and add the
    ADMIN_NEXT.MEDIA.METADATA translation strings.

--- app/_app/immutable/chunks/CQa0dWz-.js
+++
@@ -1,1 +0,0 @@
-import"./DsnmJJEf.js";import{e,f as i,a as m}from"./lkfpC_tr.js";import{s as n}from"./DwwDK4iQ.js";function c(r,o){var a=e(),t=i(a);n(t,()=>o.children),m(r,a)}export{c as L};
---
+++ app/_app/immutable/chunks/DKw_z793.js
@@ -0,0 +1,1 @@
+import"./DsnmJJEf.js";import{e,f as i,a as m}from"./IfK3kv7F.js";import{s as n}from"./BPO3vBEd.js";function c(r,o){var a=e(),t=i(a);n(t,()=>o.children),m(r,a)}export{c as L};
