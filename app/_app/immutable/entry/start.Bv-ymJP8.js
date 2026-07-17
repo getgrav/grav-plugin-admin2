0e1cbf24797e5ee65d2f9ed77951638c7277d7b2
Andy Miller <rhuk@mac.com>
2026-07-17 08:33:03 -0600

    Rebuild bundle: fix folder rename/delete under a hidden Home (#132)
    Ships the admin-next fix that addresses pages by their structural route
    when the Home page is hidden from URLs, so renaming a folder or deleting
    a page beneath Home no longer fails or relocates the page to the site
    root. Requires API plugin 1.0.12.

---
+++ app/_app/immutable/entry/start.Bv-ymJP8.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BguK3eMm.js";import{y as t}from"../chunks/XdXVhAdm.js";export{t as load_css,r as start};
--- app/_app/immutable/entry/start.v_3CTGch.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/eCCmuRKN.js";import{y as t}from"../chunks/Dmm_ymSi.js";export{t as load_css,r as start};
