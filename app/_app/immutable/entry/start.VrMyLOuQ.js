8ef1d3fc463d13439ef90dddac35319dad27e260
Andy Miller <rhuk@mac.com>
2026-07-25 12:30:28 +0100

    Rebuild admin-next bundle: collapsed-sidebar logo/avatar + permission deletions (admin2#141, #142)

--- app/_app/immutable/entry/start.VrMyLOuQ.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/b640qtcS.js";import{y as t}from"../chunks/CbLU0h-c.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.ntrqjvOf.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BCu6PYO-.js";import{y as t}from"../chunks/DXU0JJLc.js";export{t as load_css,r as start};
