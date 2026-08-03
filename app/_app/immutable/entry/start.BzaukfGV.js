52ff810dd828bcd7475ee042f1e5ef48e57ff40b
Andy Miller <rhuk@mac.com>
2026-08-03 13:04:26 +0100

    Rebuild admin-next bundle
    Picks up blueprint forms rendering as real <form> elements, so password fields
    are no longer orphaned from a form.

--- app/_app/immutable/entry/start.BzaukfGV.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CTkI_59c.js";import{y as t}from"../chunks/h9-ML6_I.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.D9LXY_s6.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/B1uPYCNo.js";import{y as t}from"../chunks/eZGFjyZm.js";export{t as load_css,r as start};
