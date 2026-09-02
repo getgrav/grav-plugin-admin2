3dfb6d00c2902c1061c3051c537145209e382e5d
Andy Miller <rhuk@mac.com>
2026-09-02 12:13:03 -0600

    Rebuild admin bundle: the sign-in screen paints the site defaults

--- app/_app/immutable/entry/start.BUB7e_yl.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DmRfqG2f.js";import{y as t}from"../chunks/DPl3XBWV.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.Bm-GuzeD.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/D94mUz05.js";import{y as t}from"../chunks/Daa-xQrF.js";export{t as load_css,r as start};
