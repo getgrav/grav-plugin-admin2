bd5c641f58fb55b918d749312a5697269b619dfe
Andy Miller <rhuk@mac.com>
2026-09-14 10:32:34 -0600

    Dashboard reads the dashboard-row notifications after dashboard, so a banner's second half can stay hidden from older admins

--- app/_app/immutable/entry/start.BVJr6Okv.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/pa5IA9H1.js";import{y as t}from"../chunks/e61_Ec9O.js";export{t as load_css,r as start};
---
+++ app/_app/immutable/entry/start.CTo0PMb5.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CjEjeix8.js";import{y as t}from"../chunks/BRC2b5_M.js";export{t as load_css,r as start};
