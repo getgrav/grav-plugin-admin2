a7c6d20ed0e23b44393ce78b120abe403b2cd938
Andy Miller <rhuk@mac.com>
2026-09-14 23:48:47 -0600

    Build 2.1.18: columns default, four-state status in all three views

---
+++ app/_app/immutable/entry/start.D9Vw0lIp.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DikjNfc-.js";import{y as t}from"../chunks/BTynw6yt.js";export{t as load_css,r as start};
--- app/_app/immutable/entry/start.DovwLqZV.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CRnEDEOc.js";import{y as t}from"../chunks/DTgR_I59.js";export{t as load_css,r as start};
