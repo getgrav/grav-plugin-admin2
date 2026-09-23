28827990ad663b9441b3b42ac6171cd98af4a311
Andy Miller <rhuk@mac.com>
2026-09-22 19:45:46 -0600

    Rebuild for admin-next f485f78: dashboard polling, font subsets, page neighbors
    Performance work for Grav 2.2.0 (Wave 2). Also adds the fast frontmatter parsing setting labels.

---
+++ app/_app/immutable/entry/start.CoNTWOXd.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/I6gXq9gU.js";import{y as t}from"../chunks/BcQXQqRL.js";export{t as load_css,r as start};
--- app/_app/immutable/entry/start.KkMveqPz.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/C63FB4sy.js";import{y as t}from"../chunks/BUfIKcC9.js";export{t as load_css,r as start};
